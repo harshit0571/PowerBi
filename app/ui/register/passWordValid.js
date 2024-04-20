8f6a3cd4715a659810b4808c89f9015fdaf1483e
Navjot Singh <109858950+navjot369@users.noreply.github.com>
2024-04-20 17:05:31 +0530

    Add example for special symbols

--- app/ui/register/passWordValid.js
+++ app/ui/register/passWordValid.js
@@ -1,13 +1,13 @@
 function ValidShow({ele}) {
     return(<div className="flex flex-col justify-center items-center">
-        <div className="bg-white bg-opacity-80 rounded-xl p-4">
+        <div className="bg-white bg-opacity-80 rounded-xl px-4">
             <ValidLine right={ele.length > 7} line="Password should be 8 characters long" />
             <ValidLine right={/\d/.test(ele)} line="Password should have digits" />
             <ValidLine right={/[a-z]/.test(ele)} line="Password should have lowercase letters" />
             <ValidLine right={/[A-Z]/.test(ele)} line="Password should have uppercase letters" />
             <ValidLine right={/[!@#$%&]/.test(ele)} line="Password should have special symbols" />
+            {/[!@#$%&]/.test(ele) ? <p className="text-green-600 text-center w-full truncate font-bold">Eg. !, @, #, $, %, &</p> : <p className="text-red-700 text-center w-full truncate font-bold">Eg. !, @, #, $, %, &</p>}
         </div>
-        <div className="w-0 h-0 border-x-8 border-t-8 border-t-white border-x-transparent opacity-80"></div>
     </div>);
 }
 
@@ -22,7 +22,7 @@ function ValidLine({right, line}) {
 
 function ConfirmShow({pass1, pass2}){
     return(<div className="flex flex-col justify-center items-center">
-    <div className="bg-white bg-opacity-80 rounded-xl p-4">
+    <div className="bg-white bg-opacity-80 rounded-xl px-4">
         {pass1 === pass2 && pass1 !== ""?
         <p className="text-green-600 truncate font-bold"><span className="px-2">&#x2713;</span>Both the passwords should be same</p>:
         <p className="text-red-700 truncate font-bold"><span className="px-2">&#x274C;</span>Both the passwords should be same</p>}
